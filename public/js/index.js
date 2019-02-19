0b36b5cab7329ce68959ecfd4a6b96346fe2317c
Russ <shtooney@yahoo.com>
2019-02-18 22:37:16 -0500

    some templating

--- public/js/index.js
+++ public/js/index.js
@@ -9,22 +9,28 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    console.log('new message: ', message);
-    let li = jQuery('<li></li>');    
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    
+    let template = jQuery('#message-template').html();
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    })
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message){
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    let a = jQuery('<a target="_blank">My Current Location</a>');
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
 
-    li.append(a);
-    jQuery('#messages').append(li);
+    let template = jQuery('#location-message-template').html();
+    let html = Mustache.render(template, {
+        URL: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    })
+
+    jQuery('#messages').append(html);
 
 });
 
